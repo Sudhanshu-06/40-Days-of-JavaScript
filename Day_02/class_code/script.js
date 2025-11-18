5821deee5ca82949fe420e3de22f65e63331bfa6
Sudhanshu-06 <sudhanshu767778@gmail.com>
2025-11-18 18:05:33 +0530

    done

---
+++ Day_02/class_code/script.js
@@ -0,0 +1,39 @@
+console.log("Day 02");
+
+// Variables: Variables are used to store data in JavaScript
+// The name must have digit (0-9) or letters
+// The name can have $ and _.
+// The first charcter must not be a digit.
+// No Reserved Keywords
+
+let $= 'dollar';
+let _= 'underscore';
+
+// Use camelcase:-
+let myName= 'Sudhanshu';
+
+
+// -var:- Function-scoped, can be redeclared (not recommended)
+// -let:- Block-scoped, can be reassigned
+// -const:- Block-scoped, cannot be reassinged
+
+// var address="Mumbai";
+// console.log(address);
+
+// var address= "Delhi";
+// console.log(address)
+
+
+// let address="Mumbai";
+// console.log(address);
+
+//  address= "Delhi";
+// console.log(address)
+
+
+const address="Mumbai";
+console.log(address);
+
+let age;
+age=12;
+
